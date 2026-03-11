92409c082392705dd96b0744252c6166a1b20972
Rizwan786 <rizwanmnhr@gmail.com>
2026-03-11 07:00:16 +0530

    change api

--- front-End/src/features/auth/services/Auth.api.jsx
+++ front-End/src/features/auth/services/Auth.api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 const api=axios.create({
-    baseURL:"http://localhost:3000/api/Instagram",
+    baseURL:"https://insta-clone-2wag.onrender.com",
     withCredentials:true
 })
 
